1624e3d36947fbbf7d0a3a990c39aa2a693c8d95
epifanio <epiesasha@me.com>
2019-09-16 14:16:37 +0200

    adding build script

---
+++ docker_build.sh
@@ -0,0 +1,7 @@
+docker build -t epinux/metsis-drupal metsis-drupal/
+docker build -t epinux/metsis-postgres metsis-postgres/
+docker build -t epinux/metsis-pybasket metsis-pybasket/
+docker build -t epinux/metsis-pydap metsis-pydap/
+docker build -t epinux/metsis-pywps metsis-pywps/
+docker build -t epinux/metsis-solr metsis-solr/
+docker build -t epinux/metsis-pybasket metsis-pybasket/
