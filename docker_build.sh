c7ceb18c7d23440b9f4c9dc4fc4478ab28652d03
epifanio <epiesasha@me.com>
2019-09-16 22:30:25 +0200

    adding non interactive debian frontend

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,7 @@
-docker build -t epinux/metsis-drupal metsis-drupal/ --no-cache
-docker build -t epinux/metsis-postgres metsis-postgres/ --no-cache
-docker build -t epinux/metsis-pybasket metsis-pybasket/ --no-cache
-docker build -t epinux/metsis-pydap metsis-pydap/ --no-cache
-docker build -t epinux/metsis-pywps metsis-pywps/ --no-cache
-docker build -t epinux/metsis-solr metsis-solr/ --no-cache
-docker build -t epinux/metsis-pybasket metsis-pybasket/ --no-cache
+docker build -t epinux/metsis-drupal metsis-drupal/ # --no-cache
+docker build -t epinux/metsis-postgres metsis-postgres/ # --no-cache
+docker build -t epinux/metsis-pybasket metsis-pybasket/ # --no-cache
+docker build -t epinux/metsis-pydap metsis-pydap/ # --no-cache
+docker build -t epinux/metsis-pywps metsis-pywps/ # --no-cache
+docker build -t epinux/metsis-solr metsis-solr/ # --no-cache
+docker build -t epinux/metsis-pybasket metsis-pybasket/ # --no-cache
