e55b87f887020243ca3e272ce5771d44659f8ff8
epifanio <epiesasha@me.com>
2019-09-24 15:11:32 +0200

    Adding demo.py flask app

---
+++ volumes/pywps/pywps-flask/demo.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2016 PyWPS Project Steering Committee
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import flask
+
+import pywps
+from pywps import Service
+
+from processes.sleep import Sleep
+from processes.ultimate_question import UltimateQuestion
+from processes.centroids import Centroids
+from processes.sayhello import SayHello
+from processes.feature_count import FeatureCount
+from processes.buffer import Buffer
+from processes.area import Area
+from processes.bboxinout import Box
+from processes.jsonprocess import TestJson
+from processes.transformation import Transformation
+
+app = flask.Flask(__name__)
+
+processes = [
+    FeatureCount(),
+    SayHello(),
+    Centroids(),
+    UltimateQuestion(),
+    Sleep(),
+    Buffer(),
+    Area(),
+    Box(),
+    TestJson(),
+    Transformations()
+]
+
+# For the process list on the home page
+
+process_descriptor = {}
+for process in processes:
+    abstract = process.abstract
+    identifier = process.identifier
+    process_descriptor[identifier] = abstract
+
+# This is, how you start PyWPS instance
+service = Service(processes, ['pywps.cfg'])
+
+
+@app.route("/")
+def hello():
+    server_url = pywps.configuration.get_config_value("server", "url")
+    request_url = flask.request.url
+    # print(app.config.keys())
+    # print(app.config['docker_port'])
+    if app.config['docker_port'] != 0:
+        print("here some actions")
+    print(server_url)
+    server_url = server_url.replace('localhost', 'localhost:' + str(app.config['docker_port']))
+    return flask.render_template('home.html', request_url=request_url,
+                                 server_url=server_url,
+                                 process_descriptor=process_descriptor)
+
+
+@app.route('/wps', methods=['GET', 'POST'])
+def wps():
+    return service
+
+
+@app.route('/outputs/' + '<path:filename>')
+def outputfile(filename):
+    targetfile = os.path.join('outputs', filename)
+    if os.path.isfile(targetfile):
+        file_ext = os.path.splitext(targetfile)[1]
+        with open(targetfile, mode='rb') as f:
+            file_bytes = f.read()
+        mime_type = None
+        if 'xml' in file_ext:
+            mime_type = 'text/xml'
+        return flask.Response(file_bytes, content_type=mime_type)
+    else:
+        flask.abort(404)
+
+
+@app.route('/static/' + '<path:filename>')
+def staticfile(filename):
+    targetfile = os.path.join('static', filename)
+    if os.path.isfile(targetfile):
+        with open(targetfile, mode='rb') as f:
+            file_bytes = f.read()
+        mime_type = None
+        return flask.Response(file_bytes, content_type=mime_type)
+    else:
+        flask.abort(404)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    test = 'tttt'
+    # Sapp = create_app(test)
+
+    parser = argparse.ArgumentParser(
+        description="""Script for starting an example PyWPS
+                       instance with sample processes""",
+        epilog="""Do not use this service in a production environment.
+         It's intended to be running in test environment only!
+        For more documentation, visit http://pywps.org/doc
+        """
+    )
+    parser.add_argument('-d', '--daemon',
+                        action='store_true', help="run in daemon mode")
+    parser.add_argument('-a', '--all-addresses',
+                        action='store_true', help="run flask using IPv4 0.0.0.0 (all network interfaces)," +
+                                                  "otherwise bind to 127.0.0.1 (localhost).  This maybe necessary in systems that only run Flask")
+    parser.add_argument('-p', '--docker-port', nargs='?', const=0, type=int, help="specify docker port")
+    args = parser.parse_args()
+    if args.docker_port:
+        app.config['docker_port'] = args.docker_port
+    if args.all_addresses:
+        bind_host = '0.0.0.0'
+    else:
+        bind_host = '127.0.0.1'
+    if args.daemon:
+        pid = None
+        try:
+            pid = os.fork()
+        except OSError as e:
+            raise Exception("%s [%d]" % (e.strerror, e.errno))
+
+        if (pid == 0):
+            os.setsid()
+            app.run(threaded=True, host=bind_host)
+        else:
+            os._exit(0)
+    else:
+        app.run(threaded=True, host=bind_host)
