bde3e2f0db4665285ac4b57a9c055be1fb1abe79
epifanio <epiesasha@me.com>
2019-10-25 14:04:14 +0200

    fixing syntax following PEP80

--- metsis-pybasket/app/solr_client.py
+++ metsis-pybasket/app/solr_client.py
@@ -11,15 +11,16 @@ class SolrClient:
         self.core_name = core_name
 
     def update(self, solr_document):
-        upadte_end_point = self.base_url + self.core_name + "/update?commit=true&wt=json"
+        update_end_point = self.base_url + self.core_name + "/update?commit=true&wt=json"
 
-        #if self.__is_url(upadte_end_point):
+        # if self.__is_url(upadte_end_point):
 
-        resp = requests.post(upadte_end_point, data=solr_document,
+        resp = requests.post(update_end_point, data=solr_document,
                              headers={'Content-Type': 'application/json', 'accept': 'application/json'})
 
         return resp.json()["responseHeader"]["status"]
 
+    # TODO: check if the method below is not used elsewhere
     def __is_url(self, url):
         try:
             result = urlparse(url)
