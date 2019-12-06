ed9581e699b333b485d1764532b3825b136fb536
epifanio <epiesasha@me.com>
2019-12-06 14:21:30 +0100

    rename pybasket to fastapi

--- docker_push.sh
+++ docker_push.sh
@@ -1,10 +1,9 @@
 docker push epinux/metsis-drupal
 docker push epinux/metsis-postgres
-docker push epinux/metsis-pybasket
+docker push epinux/metsis-fastapi
 docker push epinux/metsis-pydap
 docker push epinux/metsis-pywps
 docker push epinux/metsis-pywps-xenial
 docker push epinux/metsis-solr
-docker push epinux/metsis-pybasket
 docker push epinux/metsis-jupyter
-# docker push epinux/metsis-pycsw
+docker push epinux/metsis-pycsw
